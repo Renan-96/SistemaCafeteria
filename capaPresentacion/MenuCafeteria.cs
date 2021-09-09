07d85dff45721aca5c1d83d1c58e4c829d1ca918
Renan-96 <renan.salcedo@virginiogomez.cl>
2021-09-08 23:28:00 -0300

    Se agregan capas de arquitectura al proyecto

---
+++ capaPresentacion/MenuCafeteria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capaPresentacion
+{
+    public partial class MenuCafeteria : Form
+    {
+        private int childFormNumber = 0;
+
+        public MenuCafeteria()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
